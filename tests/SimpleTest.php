5de1c18075b20a0ff6bcff47e23cd990a001237f
David Marland <davidm@rland.me.uk>
2017-05-26 09:50:26 +0100

    Added example test

---
+++ tests/SimpleTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types = 1);
+namespace Tests\App;
+
+use PHPUnit\Framework\TestCase;
+
+class SimpleTest extends TestCase
+{
+    public function testTheUniverse()
+    {
+        $this->assertTrue(true);
+    }
+}
