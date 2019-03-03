6270c42826237aea39748428fe98fad8c39e87f6
David Marland <davidm@rland.me.uk>
2019-03-03 16:41:11 +0000

    Test Continuous Delivery again

--- src/Controller/Home/StatusAction.php
+++ src/Controller/Home/StatusAction.php
@@ -55,7 +55,7 @@ class StatusAction
             'environment' => $this->applicationConfig->getEnvironment(),
             'cache' => $this->getCacheStatus(),
             'maxScore' => PHP_INT_MAX,
-            'release' => 'PreArctan',
+            'release' => 'Alpha',
             'version' => $this->applicationConfig->getVersion(),
             'distanceMultiplier' => $this->applicationConfig->getDistanceMultiplier(),
             'schema' => $this->getMigrationStatus(),
