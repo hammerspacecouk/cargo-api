2bcb75289fe72ca3e489eba9354b23197ac9a6b3
David Marland <davidm@rland.me.uk>
2020-03-29 15:13:24 +0100

    Both console and index need to check the environment

---
+++ bin/env.php
@@ -0,0 +1,23 @@
+<?php
+
+use Symfony\Component\Dotenv\Dotenv;
+use function App\Functions\DateTimes\jsonDecode;
+
+date_default_timezone_set('UTC'); // servers should always be UTC
+
+$dotenv = new Dotenv();
+if (isset($_SERVER['APP_CONFIG'])) {
+    $vars = jsonDecode($_SERVER['APP_CONFIG']);
+    unset($_SERVER['APP_CONFIG']);
+    $dotenv->populate($vars);
+}
+if (isset($_SERVER['APP_SECRETS'])) {
+    $vars = jsonDecode($_SERVER['APP_SECRETS']);
+    unset($_SERVER['APP_SECRETS']);
+    $dotenv->populate($vars);
+}
+
+// The check is to ensure we don't use .env in production
+if (!isset($_SERVER['APP_ENV']) || $_SERVER['APP_ENV'] === 'dev') {
+    $dotenv->load(__DIR__ . '/../.env');
+}
--- public/index.php
+++ public/index.php
@@ -2,31 +2,13 @@
 declare(strict_types=1);
 
 use App\Kernel;
-use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
-use function App\Functions\DateTimes\jsonDecode;
 
 require __DIR__ . '/../vendor/autoload.php';
+require '../bin/env.php';
 
 header_remove('X-Powered-By');
-
-$dotenv = new Dotenv();
-if (isset($_SERVER['APP_CONFIG'])) {
-    $vars = jsonDecode($_SERVER['APP_CONFIG']);
-    unset($_SERVER['APP_CONFIG']);
-    $dotenv->populate($vars);
-}
-if (isset($_SERVER['APP_SECRETS'])) {
-    $vars = jsonDecode($_SERVER['APP_SECRETS']);
-    unset($_SERVER['APP_SECRETS']);
-    $dotenv->populate($vars);
-}
-
-// The check is to ensure we don't use .env in production
-if (!isset($_SERVER['APP_ENV']) || $_SERVER['APP_ENV'] === 'dev') {
-    $dotenv->load(__DIR__ . '/../.env');
-}
 
 if (($_SERVER['APP_DEBUG'] ?? ('prod' !== ($_SERVER['APP_ENV'] ?? 'dev')))) {
     umask(0000);
