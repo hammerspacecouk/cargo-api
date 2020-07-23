7c1798acc2532f4eaadb1420308cfd7502e019fc
David Marland <davidm@rland.me.uk>
2020-07-23 22:38:33 +0100

    Dials schema

---
+++ src/Domain/ValueObject/Market.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Domain\ValueObject;
+
+use App\Infrastructure\DateTimeFactory;
+use DateTimeImmutable;
+
+class Score implements \JsonSerializable
+{
+    private int $score;
+    private int $scoreRate;
+    private DateTimeImmutable $calculationTime;
+
+    public function __construct(
+        int $score,
+        int $scoreRate,
+        DateTimeImmutable $calculationTime
+    ) {
+        $this->score = $score;
+        $this->scoreRate = $scoreRate;
+        $this->calculationTime = $calculationTime;
+    }
+
+    public function getScore():int
+    {
+        return $this->score;
+    }
+
+    public function getRate(): int
+    {
+        return $this->scoreRate;
+    }
+
+    public function getCalculationTime(): DateTimeImmutable
+    {
+        return $this->calculationTime;
+    }
+
+    /**
+     * @return array<string, mixed>
+     */
+    public function jsonSerialize(): array
+    {
+        return [
+            'type' => 'Score',
+            'value' => $this->getScore(),
+            'rate' => $this->getRate(),
+            'datetime' => DateTimeFactory::toJson($this->getCalculationTime()),
+        ];
+    }
+}
