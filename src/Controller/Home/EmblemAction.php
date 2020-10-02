887f05414445d8ee1d48afe259b81b2d096a59ac
David Marland <davidm@rland.me.uk>
2020-10-02 21:53:06 +0100

    Code Style

--- src/Controller/Home/EmblemAction.php
+++ src/Controller/Home/EmblemAction.php
@@ -20,7 +20,8 @@ class EmblemAction
         return new Route('/emblem/{uuid}-{hash}.svg', [
             '_controller' => self::class,
         ], [
-            'uuid' => '^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$', // TODO - Symfony 5.2 // (new GenericValidator())->getPattern(),
+            'uuid' => '^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$',
+            // TODO - Symfony 5.2 // (new GenericValidator())->getPattern(),
             'hash' => '^[0-9a-f]{32}$',
         ]);
     }
